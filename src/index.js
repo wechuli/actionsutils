8abf279f5166b0f0af0d66c4dc9c3818d53415fa
wechuli <wechulipaul@yahoo.com>
2023-01-05 18:10:15 +0300

    add index.js code

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const core = require("@actions/core");
+const PullRequests = require("./pr/pull_requests");
+
+async function run() {
+  try {
+    let token = core.getInput("token");
+    let repoFull = process.env["GITHUB_REPOSITORY"];
+    let [owner, repo] = repoFull.split("/");
+
+    let pullRequests = new PullRequests(owner, repo, token);
+    await pullRequests.getAllPullRequests();
+    console.log(pullRequests.pulls);
+    
+    await pullRequests.filterBehindPullREquests();
+    //console.log(pullRequests.pull_requests);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
