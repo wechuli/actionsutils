4f63c47e9c44c75c6e13e073be90101a441e6694
wechuli <wechulipaul@yahoo.com>
2023-01-06 18:21:08 +0300

    better logging

--- src/tasks/context.js
+++ src/tasks/context.js
@@ -2,8 +2,13 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function context() {
-  const context = github.context;
+  const context = JSON.stringify(github.context);
+
+  console.log(".................CONTEXT..........................");
+
   console.info(context);
+
+  console.log("............................................");
 }
 
 module.exports = {
--- src/tasks/oidctoken.js
+++ src/tasks/oidctoken.js
@@ -5,7 +5,10 @@ async function oidctoken() {
   const id_token = await core.getIDToken();
   // decode the id_token
   const decoded = jsonwebtoken.decode(id_token, { complete: true });
+
+  console.info("..................ID_TOKEN..........................");
   console.info(decoded);
+  console.info("...........................................");
 }
 
 module.exports = {
