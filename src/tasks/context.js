a203b72857b295ee69b4b4c7b80a3d5bf9f3540a
wechuli <wechulipaul@yahoo.com>
2023-01-06 18:25:08 +0300

    pretty print json

--- src/tasks/context.js
+++ src/tasks/context.js
@@ -2,7 +2,7 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function context() {
-  const context = JSON.stringify(github.context);
+  const context = JSON.stringify(github.context, null, 2);
 
   console.log(".................CONTEXT..........................");
 
