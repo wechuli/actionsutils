4ce6965cf566a65533bf414b6d0383ddee05b57b
wechuli <wechuli@users.noreply.github.com>
2023-01-09 08:05:33 +0000

    Action build at commit 1aa1e747d5fca1a7b3a800729b08dbd96b66f754

--- dist/index.js
+++ dist/index.js
@@ -31761,6 +31761,11 @@ module.exports = {
 function logEnvs() {
   let allEnvs = process.env;
   JSON.stringify(allEnvs, null, 2);
+  console.log(".................ENV..........................");
+
+  console.info(allEnvs);
+
+  console.log("............................................");
 }
 
 module.exports = {
