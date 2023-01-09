2580e9ae18e1f0acf0479593bc48868b933b1d7a
wechuli <wechulipaul@yahoo.com>
2023-01-09 11:04:53 +0300

    log envs

--- src/tasks/envs.js
+++ src/tasks/envs.js
@@ -1,6 +1,11 @@
 function logEnvs() {
   let allEnvs = process.env;
   JSON.stringify(allEnvs, null, 2);
+  console.log(".................ENV..........................");
+
+  console.info(allEnvs);
+
+  console.log("............................................");
 }
 
 module.exports = {
