dcf3e1165c5a3b708cfc278b13566ab382f08486
wechuli <wechuli@users.noreply.github.com>
2023-01-13 05:52:18 +0000

    Action build at commit 6bbc525c2fcde42f0c0a1aa614a3256ec769d9c4

--- dist/index.js
+++ dist/index.js
@@ -38528,8 +38528,11 @@ function parseTaskInput() {
   // validate that we support all the tasks
   for (const task of tasksArray) {
     if (!allTasks[task]) {
-      // remove this task from the array
-      throw new Error(`Unsupported task: ${task}`);
+      throw new Error(
+        `Unsupported task: ${task}, only the following tasks are supported: ${Object.keys(
+          allTasks
+        ).join(", ")}`
+      );
     }
   }
   return tasksArray;
