3184f6300adf2af4aa45d9534336eb9f71b22c61
wechuli <wechuli@users.noreply.github.com>
2023-01-13 05:49:44 +0000

    Action build at commit ee8084941a7196b02d892b4d983659fe1fc61145

--- dist/index.js
+++ dist/index.js
@@ -38768,8 +38768,13 @@ async function run() {
       let value = await alltasks[task]();
       allTaskData[task] = value;
       // print the values in the steps
-      console.log(`Task: ${task}`);
+      console.log(
+        `.....................${task.toUpperCase()}.....................`
+      );
       console.log(value);
+      console.log(
+        `................................................................`
+      );
     }
     // create an artifact if requested
     if (determineCreateArtifact()) {
