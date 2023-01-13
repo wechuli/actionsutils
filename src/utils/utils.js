6f152779c0aa412f6220c48d79f815574f2fcd72
wechuli <wechulipaul@yahoo.com>
2023-01-13 08:51:45 +0300

    better error message if task is unsuported.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -8,8 +8,11 @@ function parseTaskInput() {
   // validate that we support all the tasks
   for (const task of tasksArray) {
     if (!allTasks[task]) {
-      // remove this task from the array
-      throw new Error(`Unsupported task: ${task}`);
+      throw new Error(
+        `Unsupported task: ${task}, only the following tasks are supported: ${Object.keys(
+          allTasks
+        ).join(", ")}`
+      );
     }
   }
   return tasksArray;
