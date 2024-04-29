fe7dc6507f809aec358a0640098766354b6c16db
wechuli <wechulipaul@yahoo.com>
2024-04-29 10:33:06 +0300

    directly evaluate audeicne

--- src/tasks/oidctoken.js
+++ src/tasks/oidctoken.js
@@ -3,7 +3,7 @@ const jsonwebtoken = require("jsonwebtoken");
 
 async function oidctoken() {
   const audience = core.getInput('audience');
-  const id_token = audience ? await core.getIDToken(audience): await core.getIDToken();
+  const id_token = await core.getIDToken(audience);
 
   // decode the id_token
   const decoded = jsonwebtoken.decode(id_token, { complete: true });
